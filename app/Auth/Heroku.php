19a122123f5f244ae97ee2613c739e9ad41e2806
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-10-11 12:58:40 +0900

    AuthURL間違っていたので修正

--- app/Auth/Heroku.php
+++ app/Auth/Heroku.php
@@ -9,7 +9,8 @@ class Heroku
         $clientId = env('HEROKU_CLIENT_ID');
         $params = [
             'client_id' => $clientId,
-            'response_tyoe' => 'code',
+            'response_type' => 'code',
+            'scope' => 'read',
         ];
         $queryString = http_build_query($params);
         $baseUrl = 'https://id.heroku.com/oauth/authorize';
