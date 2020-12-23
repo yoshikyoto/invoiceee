2385cc474fe0bf8d87ce4c31742175dcd8370956
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-12-23 12:23:15 +0900

    結合テストを追加

--- tests/Feature/AccountLinkTest.php
+++ tests/Feature/AccountLinkTest.php
@@ -3,64 +3,146 @@
 namespace Tests\Feature;
 
 use App\AbstractFactory\HttpClientFactory;
+use App\AbstractFactory\LoggerFactory;
+use App\Auth\OAuth2Token;
+use App\User\UserAccountLinker;
+use App\User\UserRepository;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Facades\App;
+use Psr\Log\LoggerInterface;
 use Tests\TestCase;
 use Mockery;
 use Mockery\MockInterface;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AccountLinkTest extends TestCase
 {
+    use RefreshDatabase;
+
     private Client $guzzleClient;
+    private LoggerInterface $logger;
+    private UserRepository $userRepository;
+
+    /**
+     * プロパティにする必要あんまり無いけど、型がつけられるのでプロパティにする
+     * @var UserAccountLinker
+     */
+    private UserAccountLinker $userAccountLinker;
+
+    private function requestOptionsWithTokenHeader(string $token): array
+    {
+        return [
+            RequestOptions::HEADERS => [
+                'Authorization' => 'Bearer ' . $token,
+            ]
+        ];
+    }
+
+    private function responseMockWithBody(string $body): Response
+    {
+        return Mockery::mock(Response::class, [
+            'getBody->getContents' => $body,
+        ]);
+    }
+
+    /**
+     * HttpClientFactory が $client を返すようにモックする
+     * @param Client $client
+     */
+    private function mockHttpClientFactoryToReturn(Client $client): void
+    {
+        $this->mock(
+            HttpClientFactory::class,
+            function (MockInterface $mock) use ($client) {
+                $mock->shouldReceive('create')
+                    ->andReturn($client);
+            }
+        );
+    }
+
+    private function mockLoggerFactoryToReturn(LoggerInterface $logger): void
+    {
+        $this->mock(
+            LoggerFactory::class,
+            function (MockInterface $mock) use ($logger) {
+                $mock->shouldReceive('create')
+                    ->andReturn($logger);
+            }
+        );
+    }
+
+    /**
+     * ログのメソッドをモックするが、ログで出力される中身は確認しない
+     */
+    private function mockLoggerMethodButNotCheckLogContent(): void
+    {
+        $this->logger->shouldReceive('info');
+    }
+
+    public function setUp(): void {
+        parent::setUp();
+        // GuzzleClient のモック
+        $this->guzzleClient = Mockery::mock(Client::class);
+        $this->mockHttpClientFactoryToReturn($this->guzzleClient);
+
+        // Logger のモック
+        $this->logger = Mockery::mock(LoggerInterface::class);
+        $this->mockLoggerFactoryToReturn($this->logger);
+
+        $this->userRepository = App::make(UserRepository::class);
+    }
 
     /**
      * @test
      */
-    public function Freeeアカウントのリンクができる()
+    public function freeeとの連携でユーザー作成とログインが行える()
     {
-        // Freee への HTTP リクエストの部分をモック
-        $this->instance(
-            HttpClientFactory::class,
-            Mockery::mock(
-                HttpClientFactory::class,
-                function (MockInterface $mock) {
-                    $this->guzzleClient = Mockery::mock(Client::class);
-                    $mock->shouldReceive('create')
-                        ->andReturn($this->guzzleClient);
+        // freee API リクエストが正常に呼び出されることを確認
+        $this->guzzleClient->shouldReceive('get')
+            ->once()
+            ->with(
+                'https://api.freee.co.jp/api/1/users/me',
+                $this->requestOptionsWithTokenHeader('tokentokentoken')
+            )->andReturn($this->responseMockWithBody(
+                '{"user":{"id":"123456"}}'
+            ));
 
-                    $uri = 'https://api.freee.co.jp/api/1/users/me';
-                    $options = [
-                        RequestOptions::HEADERS => [
-                            'Authorization' => 'Bearer tokentokentoken',
-                        ]
-                    ];
-                    $responseBody = <<<JSON
-{
-  "access_token":"tokentokentoken"
-}
-JSON;
-                    $response = Mockery::mock(Response::class, [
-                        'getBody->getContents' => $responseBody,
-                    ]);
-                    // TODO 呼び出し回数をアサーションする
-                    $this->guzzleClient->shouldReceive('get')
-                        ->with($uri, $options)
-                        ->andReturn($response = Mockery::mock(Response::class));
-            })
-        );
+        $this->mockLoggerMethodButNotCheckLogContent();
 
-        // TODO ログをモック
+        // UserAccountLinker を動かす
+        $token = new OAuth2Token('tokentokentoken');
+        $this->userAccountLinker = App::make(UserAccountLinker::class);
+        $user = $this->userAccountLinker->getOrCreateUserWithFreeeToken($token);
 
-        $response = $this->get('/freee/callback?code=codecodecode');
-        // $response->dump();
-        $response->assertStatus(200);
-    }
+        // 初めての freee 連携なのでアカウントが作成されている
+        $this->assertDatabaseHas('users', [
+            'id' => $user->getId(),
+        ]);
+        $this->assertDatabaseHas('freee_users', [
+            'id' => $user->getId(),
+            'freee_user_id' => '123456',
+            'freee_token' => 'tokentokentoken',
+        ]);
 
-    public function guzzleClientShouldReceiveFreeeTokenWithCode()
-    {
-        $uri = 'https://accounts.secure.freee.co.jp/public_api/token';
-        // TODO ちゃんと env をモックしてやる
-        $options = Mockery::any();
+        // もう一回 freee と OAuth 連携すると、
+        // invoiceee のアカウントはすでに存在しているので、
+        // token が更新されるだけになる
+        $this->guzzleClient->shouldReceive('get')
+            ->once()
+            ->with(
+                'https://api.freee.co.jp/api/1/users/me',
+                $this->requestOptionsWithTokenHeader('new_token')
+            )->andReturn($this->responseMockWithBody(
+                '{"user":{"id":"123456"}}'
+            ));
+        $newToken = new OAuth2Token('new_token');
+        $this->userAccountLinker->getOrCreateUserWithFreeeToken($newToken);
+        $this->assertDatabaseHas('freee_users', [
+            'id' => $user->getId(),
+            'freee_user_id' => '123456',
+            'freee_token' => 'new_token',
+        ]);
     }
 }
